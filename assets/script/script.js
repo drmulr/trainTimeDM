3854bf94a297ecc82fc1272552a0f57e8f37df58
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-09-05 19:18:05 -0400

    FB connected, single train loads

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,52 +1,60 @@
 
 var config = {
-  apiKey: "AIzaSyC4a0HpUaFFTgi-BL7ZgkK5cuAuIRmxdw8",
-  authDomain: "mullup-d4b17.firebaseapp.com",
-  databaseURL: "https://mullup-d4b17.firebaseio.com",
-  projectId: "mullup-d4b17",
-  storageBucket: "mullup-d4b17.appspot.com",
-  messagingSenderId: "133159445436"
-};
-firebase.initializeApp(config);
-  var db = firebase.database();
-  var name = "";
-  var email = "";
-  var age = 0;
-  var comment = "";
+    apiKey: "AIzaSyB3CMdmeXYDz1sDFpj4JieJ6Cjvi9ehuAU",
+    authDomain: "trainscheduledm-70315.firebaseapp.com",
+    databaseURL: "https://trainscheduledm-70315.firebaseio.com",
+    projectId: "trainscheduledm-70315",
+    storageBucket: "",
+    messagingSenderId: "415415412563"
+  };
+  firebase.initializeApp(config);
+
+//config data accurate for trains above.
+
+
+    var db = firebase.database();
+    var trainName = "";
+    var destination = "";
+    var frequency = 0;
+    var nextArr = 0;
+    var minAway = 0;
 
 
 //click submit. grabbing the values of each input, assigning them a variable on click
-  $("#addUser").on("click", function() {
-    event.preventDefault();
-    name = $("#nameInput").val().trim();
-    email = $("#emailInput").val().trim();
-    age = $("#ageInput").val().trim();
-    comment = $("#commentInput").val().trim();
-//throwing that information at firebase to hold on  to
-    // db.ref().set({
-      //instead of set, we need to use something else to get all members' info...
+  $("#addTrain").on("click", function() {
+      event.preventDefault();
+      trainName = $("#nameInput").val().trim();
+      destination = $("#destinationInput").val().trim();
+      firstTrainTime = $("#timeInput").val().trim();
+      frequency = $("#frequencyInput").val().trim();
+
       //need use push - allows us to store data within unique keys
-    db.ref().push({
-      name: name,
-      email: email,
-      age: age,
-      comment: comment,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+      db.ref().set({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+
+      console.log(trainName);
+      console.log(destination);
+      console.log(firstTrainTime);
+      console.log(frequency);
   });
 //lastly, adding the well at bottom, and listing all the cats:
-  // db.ref().on("child_added",function(snapshot){
-  //   $(".well").append("<p>" + snapshot.val().name + "</p");
-  //   $(".well").append("<p>" + snapshot.val().email + "</p");
-  //   $(".well").append("hr");
-  // })
+  db.ref().on("value",function(snapshot){
+    console.log("Test Snapshot: " + JSON.stringify(snapshot));
+    // $(".schedTable").append('<tr><td>test 2</td></tr>')
+    $(".schedTable").append("<tr>" + "<td>" + snapshot.val().trainName + "</td>" + "<td>" + snapshot.val().destination + "</td>" + "<td>" + snapshot.val().frequency + "</td>" + "</tr>");
+  })
 
 //grabs that data, throws it into the html from firebase!
 // db.ref().on("value",function(snapshot){
 //orderByChild = orders by date added...
-  db.ref().orderByChild("dateAdded").limitToLast(1).on("child_added",function(snapshot){
-    $("#nameDisplay").html(snapshot.val().name);
-    $("#emailDisplay").html(snapshot.val().email);
-    $("#ageDisplay").html(snapshot.val().age);
-    $("#commentDisplay").html(snapshot.val().comment);
-  });
+  // db.ref().orderByChild("dateAdded").limitToLast(1).on("child_added",function(snapshot){
+  //   $("#nameDisplay").html(snapshot.val().name);
+  //   $("#emailDisplay").html(snapshot.val().email);
+  //   $("#ageDisplay").html(snapshot.val().age);
+  //   $("#commentDisplay").html(snapshot.val().comment);
+  // });
