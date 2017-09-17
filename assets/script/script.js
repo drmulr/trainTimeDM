4398f5180b8e493e5913cd8c2909c22f18056c9f
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-09-17 15:56:11 -0400

    Added Train Time Logic

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,13 @@
 
-var config = {
-    apiKey: "AIzaSyB3CMdmeXYDz1sDFpj4JieJ6Cjvi9ehuAU",
-    authDomain: "trainscheduledm-70315.firebaseapp.com",
-    databaseURL: "https://trainscheduledm-70315.firebaseio.com",
-    projectId: "trainscheduledm-70315",
-    storageBucket: "",
-    messagingSenderId: "415415412563"
-  };
-  firebase.initializeApp(config);
+  var config = {
+      apiKey: "AIzaSyB3CMdmeXYDz1sDFpj4JieJ6Cjvi9ehuAU",
+      authDomain: "trainscheduledm-70315.firebaseapp.com",
+      databaseURL: "https://trainscheduledm-70315.firebaseio.com",
+      projectId: "trainscheduledm-70315",
+      storageBucket: "",
+      messagingSenderId: "415415412563"
+    };
+    firebase.initializeApp(config);
 
 //config data accurate for trains above.
 
@@ -40,7 +40,7 @@ var config = {
       console.log(frequency);
   });
 
-  db.ref().orderByChild("dateAdded").limitToLast(1).on("child_added",function(snapshot){
+  db.ref().orderByChild("dateAdded").limitToLast(5).on("child_added",function(snapshot){
       var tName = snapshot.val().trainName;
       var dest = snapshot.val().destination;
       var freq = snapshot.val().frequency;
@@ -50,28 +50,50 @@ var config = {
       var first = moment(snapshot.val().firstTrainTime, "hmm").format("HH:mm");
         console.log("First Train Time: " + first);
       //Current Time:
-      var now = moment().format("HH:mm");
+      var now = moment().format("HH:mm:ss");
         console.log("Now: " + now);
-        console.log("Now + interval: " + moment().add(freq, 'm').format("HH:mm"));
+        console.log("Now + interval: " + moment().add(freq, 'm').format("HH:mm:ss"));
+
+      var m = moment([7, 15, 16]);
+      console.log("To String: " + m.toString());
+
+      console.log(moment().add("minute", now).toString());
+
+      // var d = moment.duration(freq, "minutes");
+      // console.log("Duration in Min: " + d);
+
+      //BELOW GRABBED FROM ACTIVITIES
+      // Assumptions
+      var tFrequency = freq;
+      // Time is 3:30 AM
+      var firstTime = first;
+      // First Time (pushed back 1 year to make sure it comes before current time)
+      var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+          console.log(firstTimeConverted);
+      // Current Time
+      var currentTime = moment();
+          console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+      // Difference between the times
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+          console.log("DIFFERENCE IN TIME: " + diffTime);
+      // Time apart (remainder)
+      var tRemainder = diffTime % tFrequency;
+          console.log(tRemainder);
+      // Minute Until Train
+      var tMinutesTillTrain = tFrequency - tRemainder;
+          console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+      // Next Train
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+          console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
-      //calculate current time
-      // var t = new Date();
-      // var time = t.getHours() + ":" + t.getMinutes();
-      // console.log("Hrs + Mins: " + time);
-      // console.log( new Date($.now()));
-      // console.log("Current Time: " + JSON.stringify(currTime));
-
-
-      // var a = moment();
-      //   console.log("Moment: " + a.format());
-      //   console.log("Now plus 1 month: " + a.add(1, "month").format());
-      // console.log("hours + min: " + a.hours() + a.minutes());
 
 
       // console.log("Test Snapshot + Date Added: " + JSON.stringify(snapshot.dateAdded));
       $(".schedTable").append("<tr>" + "<td>" + tName + "</td>" + "<td>" + dest + "</td>" + "<td>" + freq +
-      "<td>"+now+"</td>" + "<td>"+(now+"+"+freq)+"</td>"+ "</tr>");
+      "<td>"+ nextTrain +"</td>" + "<td>"+ tMinutesTillTrain +"</td>"+ "</tr>");
 
       //need calculate future time based on firstTrainTime and frequency
+
+
   })
