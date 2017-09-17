ce459a6526d13b44f2f2095f86addca90dad6130
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-09-17 19:49:50 -0400

    Clear form after submit

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,6 +16,13 @@
     var nextArr = 0;
     var minAway = 0;
 
+    // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // // Handle Errors here.
+    // var errorCode = error.code;
+    // var errorMessage = error.message;
+    // // ...
+    // });
+
   //adding on click event - grabbing inputs from form inputs:
   $("#addTrain").on("click", function() {
       event.preventDefault();
@@ -32,6 +39,10 @@
           frequency: frequency,
           dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
+      $("#nameInput").val("");
+      $("#destinationInput").val("");
+      $("#timeInput").val("");
+      $("#frequencyInput").val("");
   });
   //adding on child_added event - each time new train added, this fires:
   //can add .limitToLast() after orderByChild
@@ -45,25 +56,24 @@
       //First Train Time:
       var first = moment(snapshot.val().firstTrainTime, "hhmm").format("HH:mm");
       // First Time (pushed back 1 year to make sure it comes before current time) in UNIX format here
-      console.log("FIRST: " + first);
-
+          // console.log("FIRST: " + first);
       var firstTimeConverted = moment(first, "hh:mm").subtract(1, "years");
-          console.log("FIRST TIME CONVERTED: " + firstTimeConverted);
+          // console.log("FIRST TIME CONVERTED: " + firstTimeConverted);
       // Current Time
       var currentTime = moment();
-          console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+          // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
       // Difference between the times
       var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-          console.log("DIFFERENCE IN TIME: " + diffTime);
+          // console.log("DIFFERENCE IN TIME: " + diffTime);
       // Time apart (remainder)
       var tRemainder = diffTime % freq;
-          console.log(tRemainder);
+          // console.log(tRemainder);
       // Minute Until Train
       var tMinutesTillTrain = freq - tRemainder;
-          console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+          // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
       // Next Train
       var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
-          console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+          // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
       // console.log("Test Snapshot + Date Added: " + JSON.stringify(snapshot.dateAdded));
       $(".schedTable").append("<tr>" +
@@ -73,4 +83,5 @@
           "<td>"+nextTrain+"</td>" +
           "<td>"+tMinutesTillTrain+"</td>" +
           "</tr>");
+      $(".spotlightTrain").html(tName + "!");
   })
